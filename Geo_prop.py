e8437581a09036698726157616efade6e0cf88c0
din0saurcat <rimazkhan04@gmail.com>
2024-05-26 20:56:54 +0200

    new

---
+++ Geo_prop.py
@@ -0,0 +1,27 @@
+
+
+
+# Geomteric properties of the beams
+
+l_b_z = 5 # length of the beam in m
+l_b_y = 0.2 # width of the beam in m
+l_b_x = 0.2 # height of the beam in m
+t = 0.02 # thickness of the beam in m
+
+# Geometric properties of the grid
+
+l_g_z = 5 # length of the grid in m
+l_g_y = 1 # width of the grid in m
+l_g_x = 0.2 # height of the grid in m
+
+
+grid_z0 = 0.5 # distance to the first grid node in m
+n_grid = 31 # number of attachement grid points
+
+I_xx = ((l_b_y)**3 * l_b_x)/12 - ((l_b_y - 2*t)**3 * (l_b_x - 2*t))/12 # moment of inertia in m^4
+I_yy = ((l_b_x)**3 * l_b_y)/12 - ((l_b_x - 2*t)**3 * (l_b_y - 2*t))/12 # moment of inertia in m^4
+I_zz = 0
+
+J_z = I_xx + I_yy # polar moment of inertia in m^4
+
+
