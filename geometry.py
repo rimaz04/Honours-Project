f33bc4f0073f89b5a8d5841418287e1253d07b15
din0saurcat <rimazkhan04@gmail.com>
2024-06-10 21:27:43 +0200

    added width

--- geometry.py
+++ geometry.py
@@ -1,7 +1,9 @@
 import numpy as np
+from matplotlib import pyplot as plt
+from scipy import interpolate
 
 
-# create function that gives alpha for a certian r
+# create function that gives alpha for a certainn r
 
 def alpha(r):
     angle = [18, 18.4, 18.8, 19, 19.1, 19.1, 19, 18.8, 18.5, 18.1, 17.6, 17, 16.2, 15.4, 14.4, 13.3, 12.1, 10.8, 9.3, 7.7, 6, 4.1, 1.9, -0.5, -3.1, -6]
@@ -14,4 +16,17 @@ def alpha(r):
     else:
         # return angle at r
         return angle_interp
+
+def width(r):
+    z1 = [0,10.975]
+    y1 = [155, 100]
+
+    z2 = [0,10.975]
+    y2 = [-155, -100]
+
+    f1 = interpolate.interp1d(z1, y1)
+    f2 = interpolate.interp1d(z2, y2)
+    w = f1(r) - f2(r)
+    return w
+
 
