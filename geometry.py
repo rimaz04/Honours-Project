30e9b9e9f2f8e886fb7ba3824c97b49c8ff4c352
sura2903 <63927720+sura2903@users.noreply.github.com>
2024-07-05 15:17:23 +0200

    update

--- geometry.py
+++ geometry.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from scipy import interpolate
 
 
-# create function that gives alpha for a certainn r
+# create function that gives alpha for a certain r
 
 def alpha(r):
     angle = [18, 18.4, 18.8, 19, 19.1, 19.1, 19, 18.8, 18.5, 18.1, 17.6, 17, 16.2, 15.4, 14.4, 13.3, 12.1, 10.8, 9.3, 7.7, 6, 4.1, 1.9, -0.5, -3.1, -6]
