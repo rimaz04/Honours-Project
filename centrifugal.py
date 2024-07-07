d96d0f8f099764b7bab7d28ca59da58f406b7758
din0saurcat <rimazkhan04@gmail.com>
2024-07-07 17:18:39 +0200

    :)

--- centrifugal.py
+++ centrifugal.py
@@ -2,18 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 from geometry import *
+from mat_prop import *
 
 def normal_force(omega, r):
     r_end = 10.975
-
-    rho = 7800 # kg/m^3
-
 
     # integrating r*dm from r to r_end
     I = integrate.quad(lambda x: rho*x*cross_sectional_area(x), r, r_end)
     n = omega**2 * I[0]
     return n
 
-print(normal_force(2.62, 0))
 
 
